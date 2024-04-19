50499e92190c4e4aa7bacde74057e6d54198da7d
Rufai Sanni <sruffy07@gmail.com>
2024-04-19 11:37:26 +0100

    create a mapper class to convert from account to accountDto

---
+++ src/main/java/com/ruffy/Bankingapp/Services/AccountServiceInterface.java
@@ -0,0 +1,7 @@
+package com.ruffy.Bankingapp.Services;
+
+import com.ruffy.Bankingapp.DTO.AccountDto;
+
+public interface AccountServiceInterface {
+    AccountDto createAccount(AccountDto account);
+}
